b75abf6f16ea0158864d313029d209a2f1d16ede
Sk-sankar <sankarkarunakaran1109.@gmail.com>
2024-10-10 10:25:57 +0530

    practice

---
+++ 08.10.24/L3practice.js
@@ -0,0 +1,49 @@
+// function number(n){
+//     for(i=1;i<=n;i++){
+//         if(i%3===0 && i%5===0){
+//             console.log("fizz Buss");
+//         }
+//         else if(i%3===0){
+//             console.log("Fizz");
+//         }
+//         else if(i%5===0){
+//             console.log("Buzz");
+//         }
+//         else{
+//             console.log(i);
+//         }
+//     }
+// }number(100);
+
+
+
+// ______________________________________________
+
+
+// function prime(num){
+//     if(num<=1){
+//         return false;
+//     }
+//     for(i=2;i<=Math.sqrt(num);i++){
+//         if(num%i===0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// function check(arr){
+//     let empty=[];
+//     for( num of arr){
+//         if(prime(num)){
+//             empty.push(num);
+//         }
+//     }
+//     return empty;
+// }
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// let result = check(arr);
+// console.log(result);
+
+
+// _____________________________________________________________________________
+
