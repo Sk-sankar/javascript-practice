f1583a948d1ed28e6d6d362e6ed0855698343893
Sankar <sankarkarunakaran1109@gmail.com>
2024-11-12 13:46:42 +0530

    practice

--- 05.11.24/l4.1.js
+++ 05.11.24/l4.1.js
@@ -1,35 +1,80 @@
-// Given a array of string return the String which has the maximum length. arr = ["Ram", "Abraham", "Peter"]
+// // Given a array of string return the String which has the maximum length. arr = ["Ram", "Abraham", "Peter"]
 
-let arr = ["Ram", "Abraham", "Peter"];
-let em = "";
+// let arr = ["Ram", "Abraham", "Peter"];
+// let em = "";
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length > em.length) {
-        em = arr[i];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length > em.length) {
+//         em = arr[i];
+//     }
+// }
+// console.log(em);
+
+
+// // ----------------
+// let arr11 = ["apple", "Ali", "mango", "Brush"];
+// let result = arr11.filter(word => word[0].toLowerCase() === 'a');
+
+// console.log(result);
+
+
+// // 
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i]; 
+//     }
+//     return reversed;
+// }
+
+
+// // 
+
+// let inputString = "hello";
+// let result1 = reverseString(inputString);
+// console.log(result1);  
+
+
+function countDistinctVowels(str) {
+    var words=str.toLowerCase();
+    var split=words.split("");
+      for(var word of split){
+        var count=[];
+        if(word==="a"||"e"||"i"||"o"||"u"){
+            
+            count.push(word);
+        }
     }
-}
-console.log(em);
+    console.log(count.length);
+   }
+   countDistinctVowels("hello")
 
-
-// ----------------
-let arr11 = ["apple", "Ali", "mango", "Brush"];
-let result = arr11.filter(word => word[0].toLowerCase() === 'a');
-
-console.log(result);
-
-
-// 
-function reverseString(str) {
-    let reversed = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i]; 
+   
+   function printStar(n) {
+    // Enter you code HERE
+    for(i=1;i<n;i++){
+        let line="";
+  
+        for(j=1;j<=n-i;j++){
+            line+=" ";
+        }
+        for(k=1;k<=i;k++){
+            line+=i+" ";
+        }
+        console.log(line);
+        
     }
-    return reversed;
-}
+    for(i=n;i>=1;i--){
+        let line="";
+        
+        for(j=1;j<=n-i;j++){
+            line+=" ";
+        }
+        for(k=1;k<=i;k++){
+            line+=i+" ";
+        }
+        console.log(line)
+    }
+    
 
-
-// 
-
-let inputString = "hello";
-let result1 = reverseString(inputString);
-console.log(result1);  
+}printStar(4)
