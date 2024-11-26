4929f18b9a278ba46412029e34d9bb5868030005
Sankar <sankarkarunakaran1109@gmail.com>
2024-11-26 09:21:21 +0530

    practicing l5

--- 18.11.24/l5day1.js
+++ 18.11.24/l5day1.js
@@ -1,15 +1,14 @@
 function bubbleSort(array) {
-    let c=0
     const n = array.length;
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 [array[j], array[j + 1]] = [array[j + 1], array[j]];
-                c++;
+                
             }
         }
     }
-    return array,c;
+    return array;
 }
 console.log(bubbleSort([2, 1, 4, 8, 3, 9]));
 
---
+++ 25.11.24/l5practice.js
@@ -0,0 +1,47 @@
+function insertion(a){
+    let n=a.length;
+    for(i=1;i<n;i++){
+       let temp= a[i];
+       j=i-1;
+       while(j>=0 && a[j]>temp){
+        a[j+1]=a[j],
+        j--;
+       }
+       a[j+1]=temp;
+    }
+    return a;
+}
+console.log(insertion([2,7,4,1,3]));
+
+
+
+
+function bubble(arr){
+    let n= arr.length;
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubble([2,7,4,1,3]));
+
+function selection(ar){
+    let n=ar.length;
+    for(i=0;i<n;i++){
+        let mid=i;
+        for(j=i+1;j<n;j++){
+            if(ar[j]<ar[mid]){
+                mid=j;
+            }
+        }
+        if(mid !==1){
+            [ar[mid],ar[i]]=[ar[i],ar[mid]]
+        }
+    }
+    return ar;
+}
+console.log(selection([2,7,4,1,3]));
