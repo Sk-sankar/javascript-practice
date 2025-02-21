62258969c5feebb848e8ef1af4ab8851046b8136
Sankar <sankarkarunakaran1109@gmail.com>
2025-02-21 10:24:25 +0530

    today learning

---
+++ 20.02.25/ps.js
@@ -0,0 +1,12 @@
+let a =[2,4,5,6,7,70,22];
+
+for(i=0;i<a.length;i++){
+    for(j=0;j<a.length;j++){
+        if(a[i]<a[j]){
+            let temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }
+}
+console.log(a)
