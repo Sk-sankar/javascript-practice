cf81e09c4f10386c594fe2bbdad919c24b8661f3
Sankar <sankarkarunakaran1109@gmail.com>
2025-04-05 10:36:45 +0530

    todays learning

---
+++ 04.04.25/practice.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    # Append remaining elements
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+# Test it
+print(merge_sort([4, 32, 53, 1, 5, 13, 124, 89, 23]))
